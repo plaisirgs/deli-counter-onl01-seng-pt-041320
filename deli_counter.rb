336edfc4e6b541c8298c8c36679dae0f318e6ffa
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-04-26 16:44:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
-# require 'pry'
+require 'pry'
 def line(deli)
   if deli.empty?
     puts "The line is currently empty."
+  elsif deli.length > 0
+    binding.pry
+    puts "The line is currently: "
+
   end
 end
